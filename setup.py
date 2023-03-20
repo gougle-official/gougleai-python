df8f685b5879ef46a6c2a3d08e1605cb364245bb
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-03-20 03:30:40 +0100

    Add basic `setup.py` to pip build.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gougleai',
+    version='1.12',
+    packages=['gougleai'],
+    install_requires=[
+
+    ],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+)
