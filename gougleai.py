099822941283d7ea58dc730918016705351db5f5
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-03-21 08:59:50 +0100

    Add request import.

--- gougleai.py
+++ gougleai.py
@@ -2,27 +2,28 @@
 # Exuecute cmd line: '"U:\prive\Applications\Python\python.exe" ./gougleai.py'
 # Install <requests> with pip: '"U:\prive\Applications\Python\Scripts\pip.exe" install requests'
 
-# import requests
+import requests
 
 apiKey = ""
 orgId = ""
 
 class models:
-	list = """GLT-1: gougleai.models.glt.glt1 (gougleai.models.glt.versions[0])"""
+	list = """GLT-1: gougleai.models.glt.glt1 (gougleai.models.glt.versions[0])
+GLT-1.0.5: gougleai.models.glt.glt105 (gougleai.models.glt.versions[1])"""
 	
 	class glt:
-		versions = [1];
 		glt1 = "gougleai.models.glt.glt1"
+		glt105 = "gougleai.model.glt.glt105"
+		versions = [glt1, glt105];
 
-def complete(model, prompt:str, tokenNumber):
+def complete(model, prompt:str, maxTokenNumber:int = 100):
 	if apiKey == "":
 		raise Exception("API Key cannot be empty string.")
 	else:
 		complete.choices = []
 		if str(model) in models.list:
 			if prompt != "":
-				# complete.choices = [requests.get('https://api.openai.com/v1/engines/davinci-codex/completions', headers = {'Content-Type': 'application/json','Authorization': f'Bearer {apiKey}'}, params = {'prompt': prompt,'max_tokens': tokenNumber}).json()]
-				complete.choices = [prompt]
+				complete.choices = [requests.get('https://api.openai.com/v1/engines/davinci-codex/completions', headers = {'Content-Type': 'application/json','Authorization': f'Bearer {apiKey}'}, params = {'prompt': prompt,'max_tokens': maxTokenNumber}).json()]
 				return complete
 			else:
 				raise Exception("Cannot complete empty string.")
