e2eb32db351b6c7e4ed167b8def8e7e18f8d5ce6
foxy pirate cove / Fnaf <108185011+foxypiratecove37350@users.noreply.github.com>
2024-03-06 02:43:23 +0100

    Update setup.py

--- gougleai/setup.py
+++ gougleai/setup.py
@@ -6,7 +6,7 @@ setup(
     author='Gougle AI LLC',
     author_email='gouglellc@gmail.com',
     description='The Python package for Gougle AI API.',
-    long_description=open('../README.md', "r").read(),
+    long_description=open('README.md', "r").read(),
     long_description_content_type='text/markdown',
     url='https://www.github.com/gougle-official/gougleai-python',
     packages=find_packages(),
