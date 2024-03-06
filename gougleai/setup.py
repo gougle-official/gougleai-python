536d6352914cd035a2f97e29ff0f2f82333a59a2
foxy pirate cove / Fnaf <108185011+foxypiratecove37350@users.noreply.github.com>
2024-03-06 02:56:29 +0100

    Update setup.py

--- gougleai/setup.py
+++ gougleai/setup.py
@@ -1,7 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 setup(
     name='gougleai',
@@ -9,7 +6,7 @@ setup(
     author='Gougle AI LLC',
     author_email='gouglellc@gmail.com',
     description='The Python package for Gougle AI API.',
-    long_description=open(os.path.join(parent_dir, 'README.md'), "r").read(),
+    long_description=open('README.md', "r").read(),
     long_description_content_type='text/markdown',
     url='https://www.github.com/gougle-official/gougleai-python',
     packages=find_packages(),
